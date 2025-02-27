74867fb68f7b76faca95525ca10e1890a680bb2e
ecekilis <ecekilis@gmail.com>
2025-02-27 21:54:02 +0100

    third

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "./Hero.module.css";
-import heroImage from "../../../assets/hero/Foto.jpg"
+import heroImage from "../../../assets/hero/Foto.JPG"
 
 const Hero = () => {
     return (
