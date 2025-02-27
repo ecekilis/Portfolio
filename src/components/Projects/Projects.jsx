faaa701dd8728742a2e90a605878fbbcb8f94154
ecekilis <ecekilis@gmail.com>
2025-02-28 00:48:57 +0100

    ece

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,7 +8,7 @@ import { ProjectCard } from "./ProjectCard";
 export const Projects = () => {
     return (
         <section className={styles.container} id="projects">
-            <h2 className={styles.title}>Projecte</h2>
+            <h2 className={styles.title}>Projekte</h2>
             <div className={styles.projects}>
                 {projects.map((project, id) => {
                     return <ProjectCard key={id} project={project} />;
